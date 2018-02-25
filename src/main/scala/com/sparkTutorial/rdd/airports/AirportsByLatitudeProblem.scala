73a46ad7825bd357dc6a496ca7c3d9a90963f887
javieku <javieku@gmail.com>
2018-02-25 22:22:13 +0100

    Solve exercise lecture 12

--- src/main/scala/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.scala
@@ -19,5 +19,17 @@ object AirportsByLatitudeProblem {
        "Tofino", 49.082222
        ...
      */
+
+    val conf = new SparkConf().setAppName("airports").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    val airports = sc.textFile("in/airports.text")
+    val airportsInUSA = airports.filter(line => line.split(Utils.COMMA_DELIMITER)(6).toDouble > 40.0)
+
+    val airportsNameAndCityNames = airportsInUSA.map(line => {
+      val splits = line.split(Utils.COMMA_DELIMITER)
+      splits(1) + ", " + splits(6)
+    })
+    airportsNameAndCityNames.saveAsTextFile("out/airports_in_usa.text")
   }
 }
