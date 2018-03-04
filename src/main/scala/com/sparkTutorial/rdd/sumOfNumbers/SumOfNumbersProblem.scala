c8d48fb6dcad3fee700d60557c7d9254ae60a0dd
javieku <javieku@gmail.com>
2018-03-04 18:45:57 +0100

    Add Spark program to compute the sum of the first 100 prime numbers

--- src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/sumOfNumbers/SumOfNumbersProblem.scala
@@ -1,5 +1,7 @@
 package com.sparkTutorial.rdd.sumOfNumbers
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SumOfNumbersProblem {
 
   def main(args: Array[String]) {
@@ -9,5 +11,20 @@ object SumOfNumbersProblem {
 
        Each row of the input file contains 10 prime numbers separated by spaces.
      */
+
+    val conf = new SparkConf().setAppName("sum-100-primes").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+
+    val primesFile = sc.textFile("in/prime_nums.text")
+
+    val numbers = primesFile.flatMap(line => line.split( "\\s+" ))
+
+    val validNumbers = numbers.filter(number => !number.isEmpty)
+
+    val primes = validNumbers.map(string => string.toInt)
+
+    val result = primes.take(100).reduce((x,y) => x + y)
+
+    println("Total sum " + result)
   }
 }
